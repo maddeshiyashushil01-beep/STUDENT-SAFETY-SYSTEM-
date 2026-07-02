e9f1657ad6dcbabac7773f1096758eec8a4d730b
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-02 12:59:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDiPJpV1V7s5xsSmrT-Isg_g3Lzehf-7I",
+  apiKey:"AIzaSyDDiPJpV1V7s5xsSmrT-Isg_g3Lzehf-7I",
   authDomain: "student-safety-system-95f81.firebaseapp.com",
   projectId: "student-safety-system-95f81",
   storageBucket: "student-safety-system-95f81.firebasestorage.app",
