447560dda35f322b2572d4db8e42222d995ae452
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-04 23:18:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -38,3 +38,34 @@ const auth =
 getAuth(app);
 
 export { db, auth };
+
+
+import { initializeApp }
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore }
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getAuth }
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "student-safety-system-95f81.firebaseapp.com",
+  projectId: "student-safety-system-95f81",
+  storageBucket: "student-safety-system-95f81.appspot.com",
+  messagingSenderId: "754374752811",
+  appId: "1:754374752811:web:d0d01789013cd2b87e886e"
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+export const db =
+getFirestore(app);
+
+export const auth =
+getAuth(app);
