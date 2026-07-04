7db6a2202a92bbc57ffa3ba2208d636c3f472078
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-04 22:59:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -18,3 +18,23 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getFirestore(app);
+
+const auth =
+getAuth(app);
+
+export { db, auth };
