dc4ac68a368c9d5ecedc48477d726c9865e374a0
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-06 13:01:59 +0530

    Update register.js

--- register.js
+++ register.js
@@ -3,9 +3,6 @@ import {
   createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const createBtn = document.getElementById("createAccountBtn");
-const registerBox = document.getElementById("registerBox");
-const registerBtn = document.getElementById("registerBtn");
 
 createBtn.addEventListener("click", () => {
   registerBox.style.display = "block";
