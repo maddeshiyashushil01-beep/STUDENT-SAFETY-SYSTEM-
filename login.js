635b44b1f49653c76d3c7760743cbdc8f5aea876
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-17 20:25:58 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,78 +1,30 @@
 import { auth } from "./firebase.js";
-
 import {
-  signInWithEmailAndPassword,
-  setPersistence,
-  browserLocalPersistence
+signInWithEmailAndPassword,
+setPersistence,
+browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const form = document.getElementById("loginForm");
+const loginForm = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 
-form.addEventListener("submit", async (e) => {
+loginForm.addEventListener("submit", async (e) => {
+e.preventDefault();
 
-  e.preventDefault();
+const email = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
 
-  const role = document.getElementById("role").value;
-  const email = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
+try {
+await setPersistence(auth, browserLocalPersistence);
 
-  errorMsg.innerText = "";
+await signInWithEmailAndPassword(auth, email, password);  
 
-  try {
+alert("Login Successful");  
 
-    await setPersistence(auth, browserLocalPersistence);
+location.replace("sos.html");
 
-    await signInWithEmailAndPassword(auth, email, password);
-
-    if (role === "admin") {
-
-      if (email === "admin@gmail.com") {
-
-        alert("Welcome Admin");
-        location.replace("admin.html");
-
-      } else {
-
-        alert("You are not an Admin.");
-        await auth.signOut();
-
-      }
-
-    } else {
-
-      alert("Welcome Student");
-      location.replace("sos.html");
-
-    }
-
-  } catch (error) {
-
-    switch (error.code) {
-
-      case "auth/invalid-credential":
-        errorMsg.innerText = "Invalid Email or Password";
-        break;
-
-      case "auth/user-not-found":
-        errorMsg.innerText = "User Not Found";
-        break;
-
-      case "auth/wrong-password":
-        errorMsg.innerText = "Wrong Password";
-        break;
-
-      case "auth/invalid-email":
-        errorMsg.innerText = "Invalid Email";
-        break;
-
-      default:
-        errorMsg.innerText = error.message;
-
-    }
-
-    console.log(error);
-
-  }
-
+} catch (error) {
+console.log(error);
+errorMsg.innerText = error.code + " : " + error.message;
+}
 });
