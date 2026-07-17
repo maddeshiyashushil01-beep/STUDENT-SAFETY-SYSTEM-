d0b525219acb0d44c3229b89702c28a922ac96c0
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-17 20:14:47 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,30 +1,78 @@
 import { auth } from "./firebase.js";
+
 import {
   signInWithEmailAndPassword,
   setPersistence,
   browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const loginForm = document.getElementById("loginForm");
+const form = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 
-loginForm.addEventListener("submit", async (e) => {
+form.addEventListener("submit", async (e) => {
+
   e.preventDefault();
 
+  const role = document.getElementById("role").value;
   const email = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
+  errorMsg.innerText = "";
+
   try {
+
     await setPersistence(auth, browserLocalPersistence);
 
     await signInWithEmailAndPassword(auth, email, password);
 
-    alert("Login Successful");
+    if (role === "admin") {
 
-    location.replace("sos.html");
+      if (email === "admin@gmail.com") {
+
+        alert("Welcome Admin");
+        location.replace("admin.html");
+
+      } else {
+
+        alert("You are not an Admin.");
+        await auth.signOut();
+
+      }
+
+    } else {
+
+      alert("Welcome Student");
+      location.replace("sos.html");
+
+    }
 
   } catch (error) {
+
+    switch (error.code) {
+
+      case "auth/invalid-credential":
+        errorMsg.innerText = "Invalid Email or Password";
+        break;
+
+      case "auth/user-not-found":
+        errorMsg.innerText = "User Not Found";
+        break;
+
+      case "auth/wrong-password":
+        errorMsg.innerText = "Wrong Password";
+        break;
+
+      case "auth/invalid-email":
+        errorMsg.innerText = "Invalid Email";
+        break;
+
+      default:
+        errorMsg.innerText = error.message;
+
+    }
+
     console.log(error);
-    errorMsg.innerText = error.code + " : " + error.message;
+
   }
+
 });
