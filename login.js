165f59f2dbf76ba315f4416c06cb551e2004f6a5
Shushil maddeshiya <maddeshiyashushil01@gmail.com>
2026-07-21 13:17:05 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,30 +1,63 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
 import {
-signInWithEmailAndPassword,
-setPersistence,
-browserLocalPersistence
+  signInWithEmailAndPassword,
+  setPersistence,
+  browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const loginForm = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 
 loginForm.addEventListener("submit", async (e) => {
-e.preventDefault();
 
-const email = document.getElementById("username").value.trim();
-const password = document.getElementById("password").value.trim();
+  e.preventDefault();
 
-try {
-await setPersistence(auth, browserLocalPersistence);
+  const email = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").trim();
+  const role = document.getElementById("role").value;
 
-await signInWithEmailAndPassword(auth, email, password);  
+  try {
 
-alert("Login Successful");  
+    await setPersistence(auth, browserLocalPersistence);
 
-location.replace("sos.html");
+    await signInWithEmailAndPassword(auth, email, password);
 
-} catch (error) {
-console.log(error);
-errorMsg.innerText = error.code + " : " + error.message;
-}
+    if(role === "admin"){
+
+      const snapshot = await getDocs(collection(db,"admins"));
+
+      let isAdmin = false;
+
+      snapshot.forEach(doc=>{
+        if(doc.data().email === email){
+          isAdmin = true;
+        }
+      });
+
+      if(isAdmin){
+        location.replace("admin.html");
+      }else{
+        alert("Access Denied");
+      }
+
+    }else{
+
+      location.replace("sos.html");
+
+    }
+
+  } catch(error){
+
+    console.log(error);
+
+    errorMsg.innerText = error.message;
+
+  }
+
 });
